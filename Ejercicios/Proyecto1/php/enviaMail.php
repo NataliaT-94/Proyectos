0cc27a6e670b4a4d92d82491e84a591b4e928ab7
NataliaT-94 <93306996+NataliaT-94@users.noreply.github.com>
2022-06-04 19:35:36 -0300

    Ejercicios

---
+++ Ejercicios/Proyecto1/php/enviaMail.php
@@ -0,0 +1,17 @@
+<?php
+// Recibo las variables de formulario
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+//Te muestro las variables
+
+echo $nombre;
+echo "<br />";
+echo $mail;
+echo "<br />";
+echo $mensajes;
+echo "<br />";
+
+?>
